90084a16262a30bf18e9fcb2035a5f77d21f8d78
Jairo Sanchez <jairosanchez@Jairos-MacBook-Air.local>
2021-02-03 10:44:00 -0800

    made multiple features
    some include getting song and setting next titile.

--- Song.py
+++ Song.py
@@ -7,22 +7,23 @@ class Song:
 
   # TODO: Create a getter method for the title attribute, called get_title
   def get_title(self):
-    pass
+    return self.__title
   
   
   # TODO: Create a setter method for the next_song attribute, called set_title. Make sure titles are type cased to strings and are Title Cased.
   def set_title(self, title):
-    pass
+    self.__title.insert(title)
+    self.__next_song = title
 
 
   # TODO: Create a getter method for the next_song attribute, called get_next_song
   def get_next_song(self):
-    pass
+    return self.__next_song
 
 
   # TODO: Create a setter method for the next_song attribute, called set_next_song
   def set_next_song(self, next_title):
-    pass
+    self.__title.insert(self.__next_song)
 
 
   # TODO: Using the __str___ dunder method, return a string of the song title.
