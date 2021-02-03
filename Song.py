0508a59168a41e2a4715c37ad599aa55b819f19c
Jairo Sanchez <jairosanchez@Jairos-MacBook-Air.local>
2021-02-03 12:17:56 -0800

    changed comments

--- Song.py
+++ Song.py
@@ -5,27 +5,27 @@ class Song:
       self.__next_song = None
 
 
-  # TODO: Create a getter method for the title attribute, called get_title
+  # Created a getter method for the title attribute to print current song title, called get_title
   def get_title(self):
     return print(self.__title)
   
   
-  # TODO: Create a setter method for the next_song attribute, called set_title. Make sure titles are type cased to strings and are Title Cased.
+  # Create a setter method for the next_song attribute stes new value for self.__title, called set_title.
   def set_title(self, title):
     self.__title = title
 
 
-  # TODO: Create a getter method for the next_song attribute, called get_next_song
+  # Created a getter method for the next_song attribute, called get_next_song
   def get_next_song(self):
     return print(self.__next_song)
 
 
-  # TODO: Create a setter method for the next_song attribute, called set_next_song
+  # Created a setter method for the next_song attribute, called set_next_song
   def set_next_song(self, next_title):
     self.__next_song = next_title
 
 
-  # TODO: Using the __str___ dunder method, return a string of the song title.
+  # Using the __str___ dunder method, return a string of the song title.
   def __str__(self):
     return print(f"Here's the song! {self.__title}" .format(self=self))
 
